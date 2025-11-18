d649c887271ade747fc88275086da4a879f8b5ac
bagotsaycg <bagotsaycg@gmail.com>
2025-11-18 10:05:11 +0800

    Add complete workout to routes

--- src/routes/workout.routes.js
+++ src/routes/workout.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addWorkout, deleteWorkout, getWorkouts, updateWorkout } from "../controllers/workout.controller.js";
+import { addWorkout, completeWorkout, deleteWorkout, getWorkouts, updateWorkout } from "../controllers/workout.controller.js";
 import { verify } from "../middleware/auth.js";
 
 
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post("/addWorkout", verify, addWorkout);
 router.get("/getMyWorkouts", verify, getWorkouts);
 router.patch("/updateWorkout/:workoutId", verify, updateWorkout);
+router.patch("/completeWorkoutStatus/:workoutId", verify, completeWorkout)
 router.delete("/deleteWorkout/:workoutId", verify, deleteWorkout);
 
 export default router;
