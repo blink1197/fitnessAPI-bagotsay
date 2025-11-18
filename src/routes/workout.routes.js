bdccc21bbdeb3f5bc1202d04226e32685031a276
bagotsaycg <bagotsaycg@gmail.com>
2025-11-18 09:54:55 +0800

    Add update workout to routes

--- src/routes/workout.routes.js
+++ src/routes/workout.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addWorkout, getWorkouts } from "../controllers/workout.controller.js";
+import { addWorkout, getWorkouts, updateWorkout } from "../controllers/workout.controller.js";
 import { verify } from "../middleware/auth.js";
 
 
@@ -8,6 +8,8 @@ const router = express.Router();
 
 router.post("/addWorkout", verify, addWorkout);
 router.get("/getMyWorkouts", verify, getWorkouts);
+
+router.patch("/updateWorkout/:workoutId", verify, updateWorkout);
 
 
 export default router;
